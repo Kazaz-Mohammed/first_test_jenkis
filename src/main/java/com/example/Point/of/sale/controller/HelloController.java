76e031e39a64a7a6bd136a39d527a8a67d7fbe33
Mohammed Kazaz <Mohamed.kazaz@emsi-edu.ma>
2025-12-19 00:34:18 +0100

    Update HelloController

--- src/main/java/com/example/Point/of/sale/controller/HelloController.java
+++ src/main/java/com/example/Point/of/sale/controller/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String hello(){
-        return "Hello new from test)";
+        return "Hello new from test 2)";
     }
 
     @GetMapping("/user")
